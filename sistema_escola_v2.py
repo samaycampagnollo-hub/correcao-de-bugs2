a4ae1d5aa2054c0421418179cf29b310c7b8e46f
samaycampagnollo-hub <samay.campagnollo@edu.senai.br>
2026-06-15 11:37:11 -0300

    # BUG: deveria ser return 1
    O 0 foi trocado pelo 1

--- sistema_escola_v2.py
+++ sistema_escola_v2.py
@@ -282,7 +282,7 @@ def fibonacci(n, memo={}):
     if n == 0:
         return 0
     if n == 1:
-        return 0   # BUG: deveria ser return 1
+        return 1   
     memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
     return memo[n]
 
