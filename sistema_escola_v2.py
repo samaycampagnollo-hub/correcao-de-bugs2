8f8530e0455a8edfa80e7353479a14099a54db35
samaycampagnollo-hub <samay.campagnollo@edu.senai.br>
2026-06-15 10:33:39 -0300

    # BUG: argumentos invertidos
    As palavras palavra_proibida, asteriscos foram invertidas

--- sistema_escola_v2.py
+++ sistema_escola_v2.py
@@ -99,7 +99,7 @@ def remover_espacos_duplos(texto):
 def censurar_palavra(texto, palavra_proibida):
     """Substitui a palavra proibida por asteriscos do mesmo tamanho."""
     asteriscos = "*" * len(palavra_proibida)
-    return texto.replace(asteriscos, palavra_proibida)  # BUG: argumentos invertidos
+    return texto.replace( palavra_proibida, asteriscos)  
 
 # Função 10 - Verifica se dois textos são anagramas
 # 🐛 BUG: compara os textos diretamente em vez de comparar as letras ordenadas
